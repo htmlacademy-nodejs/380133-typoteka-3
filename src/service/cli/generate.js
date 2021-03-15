a850dc861d5d2bc316c62d01fbc4c4690db23616
IerominSergii <iconboooook@gmail.com>
2021-03-15 21:59:16 +0200

    Add async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,4 +1,4 @@
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {getRandomInt, shuffle, getRandomArticleDate, getRandomElement} = require(`../../utils`);
 const {
@@ -14,7 +14,6 @@ const {
   GENERATE_ERROR_MESSAGE,
   GENERATE_SUCCESS_MESSAGE,
 } = require(`../constants`);
-
 
 const generateTitle = () => getRandomElement(TITLES);
 
@@ -43,20 +42,17 @@ const generateArticles = (count = DEFAULT_COUNT) =>
       createdDate: getRandomArticleDate(),
     }));
 
-const writeFile = (articles, cb) =>
-  fs.writeFile(FILE_NAME, JSON.stringify(articles), cb);
+const writeFile = async (fileName, articles) => {
+  try {
+    await fs.writeFile(fileName, JSON.stringify(articles));
 
-const exitProgram = (err) => {
-  if (err) {
-    console.info(chalk.red(GENERATE_ERROR_MESSAGE));
-    process.exit(ExitCode.ERROR);
+    console.info(chalk.green(GENERATE_SUCCESS_MESSAGE));
+  } catch (error) {
+    console.info(chalk.red(GENERATE_ERROR_MESSAGE, error));
   }
-
-  console.info(chalk.green(GENERATE_SUCCESS_MESSAGE));
-  process.exit(ExitCode.SUCCESS);
 };
 
-const generate = (args) => {
+const generate = async (args) => {
   const [count] = args;
   const countArticles = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -66,12 +62,17 @@ const generate = (args) => {
   }
 
   const articles = generateArticles(countArticles);
-  writeFile(articles, exitProgram);
+  try {
+    await writeFile(FILE_NAME, articles);
+    process.exit(ExitCode.SUCCESS);
+  } catch (error) {
+    process.exit(ExitCode.ERROR);
+  }
 };
 
 module.exports = {
   name: `--generate`,
-  run(args) {
-    generate(args);
+  async run(args) {
+    await generate(args);
   },
 };
