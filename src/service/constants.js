908ddb08a8eadd903cc6c99ab88270eb9023dc75
IerominSergii <iconboooook@gmail.com>
2021-03-16 23:02:40 +0200

    Add first server

--- src/service/constants.js
+++ src/service/constants.js
@@ -15,6 +15,20 @@ const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
+// server
+const DEFAULT_PORT = 3000;
+const SERVER_ERROR_MESSAGE = `Ошибка при создании сервера`;
+const SERVER_SUCCESS_MESSAGE = `Ожидаю соединений на `;
+const NOT_FOUND_MESSAGE = `Not found`;
+const HttpCode = {
+  OK: 200,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+
 module.exports = {
   DEFAULT_COUNT,
   MAX_COUNT,
@@ -27,4 +41,9 @@ module.exports = {
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
   FILE_CATEGORIES_PATH,
+  DEFAULT_PORT,
+  SERVER_ERROR_MESSAGE,
+  SERVER_SUCCESS_MESSAGE,
+  NOT_FOUND_MESSAGE,
+  HttpCode
 };
