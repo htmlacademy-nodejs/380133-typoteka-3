efeed4b8760f8a80a9cc64ad29ec9afb25845a4a
IerominSergii <iconboooook@gmail.com>
2021-03-20 10:32:38 +0200

    Render new-post page as well

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -2,9 +2,9 @@ const {Router} = require(`express`);
 
 const articlesRouter = new Router();
 
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRouter.get(`/add`, (req, res) => res.render(`./components/new-post`));
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`./components/articles-by-category`));
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
 articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
+articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
 
 module.exports = articlesRouter;
