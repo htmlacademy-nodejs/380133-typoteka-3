a3d6766173043d4d4d6b350f424f5dd7133e6b31
IerominSergii <iconboooook@gmail.com>
2021-03-20 11:16:32 +0200

    Another server

---
+++ src/service/cli/routes.js
@@ -0,0 +1,27 @@
+const {Router} = require(`express`);
+const chalk = require(`chalk`);
+const fs = require(`fs`).promises;
+const {
+  FILE_NAME,
+  NOT_FOUND_MESSAGE,
+} = require(`../constants`);
+
+const router = new Router();
+
+router.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(`./${FILE_NAME}`);
+    const mocks = JSON.parse(fileContent);
+
+    return res.send(mocks);
+  } catch (error) {
+    console.info(chalk.red(NOT_FOUND_MESSAGE, error));
+    return res.send([]);
+  }
+});
+
+const routes = (app) => {
+  app.use(`/`, router);
+};
+
+module.exports = routes;
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,71 +1,34 @@
 
+const express = require(`express`);
 const chalk = require(`chalk`);
-const http = require(`http`);
-const fs = require(`fs`).promises;
+const routes = require(`./routes`);
 const {
-  FILE_NAME,
   DEFAULT_PORT,
   SERVER_ERROR_MESSAGE,
   SERVER_SUCCESS_MESSAGE,
-  NOT_FOUND_MESSAGE,
-  HttpCode,
 } = require(`../constants`);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-  <!Doctype html>
-<html lang="ru">
-<head>
-<title>With love from Node</title>
-</head>
-<body>${message}</body>
-</html>`.trim();
 
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
+const server = () => {
+  const app = express();
+
+  app.use(express.json());
+
+  // routes
+  routes(app);
+
+  app.listen(DEFAULT_PORT, (error) => {
+    if (error) {
+      console.info(chalk.red(SERVER_ERROR_MESSAGE, error));
+    }
+
+    console.info(chalk.green(SERVER_SUCCESS_MESSAGE, DEFAULT_PORT));
   });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(`./${FILE_NAME}`);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((article) => `<li>${article.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (error) {
-        sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_MESSAGE);
-      }
-      break;
-
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_MESSAGE);
-      break;
-  }
-};
-
-const server = (args) => {
-  const [customPort] = args;
-  const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (error) => {
-      if (error) {
-        console.info(chalk.red(SERVER_ERROR_MESSAGE, error));
-      }
-
-      console.info(chalk.green(SERVER_SUCCESS_MESSAGE, port));
-    });
 };
 
 module.exports = {
   name: `--server`,
-  run(args) {
-    server(args);
+  run() {
+    server();
   }
 };
