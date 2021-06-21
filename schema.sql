3c565da8526776757f43a4c397985de493afa7f5
Sergii Yeromin <iconboooook@gmail.com>
2021-06-21 20:18:33 +0300

    Личный проект: приручить слона (#14)
    * Add schema.sql
    
    * Add is_author property

---
+++ schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS catgories;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS articles_categories;
+
+CREATE TABLE catgories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email varchar(255) UNIQUE NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  avatar varchar(50),
+  is_author boolean DEFAULT false
+);
+
+CREATE TABLE articles(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title varchar(255) NOT NULL,
+  announce text NOT NULL,
+  full_text text NOT NULL,
+  created_date timestamp DEFAULT current_timestamp,
+  user_id integer NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  created_date timestamp DEFAULT current_timestamp,
+  text text NOT NULL,
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE articles_categories(
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  PRIMARY KEY (article_id, user_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE INDEX ON articles (title);
